f5f6e931f6c712065c34f5d1dd358e7a161532d3
KieranWardle <kieran.wardle@ons.gov.uk>
2018-05-16 15:42:08 +0100

    Added base mvn spring project

---
+++ src/main/java/uk/gov/ons/ledr/redmat/ledrredmatpoc/LedrRedmatPocApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.ons.ledr.redmat.ledrredmatpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedrRedmatPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedrRedmatPocApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/gov/ons/ledr/redmat/ledrredmatpoc/LedrRedmatPocApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.gov.ons.ledr.redmat.ledrredmatpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LedrRedmatPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
